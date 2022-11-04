fdf0af7a85f313bd3b1e8560b5eb73bb05993f10
Wheeljack <julien.dubrail@auto1.com>
2022-11-04 15:20:24 +0100

    link to product form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post '/products', to:'products#create'
-  get '/products/new', to:'products#new'
+  get '/products/new', to:'products#new', as: :new_product
   get '/products', to:'products#index'
   get '/products/:id', to:'products#show'
 end
