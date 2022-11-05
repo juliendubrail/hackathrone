f8c1f58a8cf8e3244f04a96787d65475ffd11899
Wheeljack <julien.dubrail@auto1.com>
2022-11-05 21:47:36 +0100

    added User login

---
+++ app/controllers/authentification/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Authentification::SessionsController < ApplicationController
+    def new
+       
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to products_path, notice: "Your account has been created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :username, :password)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   namespace :authentification, path: '', as: '' do
     resources  :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create]
   end
 end
