413e62939711bb67b0ea17f0d4b7422b7b338ff1
Wheeljack <julien.dubrail@auto1.com>
2022-11-05 02:03:21 +0100

    active storage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,15 +15,31 @@ class ProductsController < ApplicationController
         @product = Product.new(product_params)
 
         if @product.save
-            redirect_to products_path
+            redirect_to products_path, notice: 'Product added correctly'
         else
-            render :new
+            render :new, status: :unprocessable_entity
         end
 
     end
 
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+
+        if @product.update(product_params)
+            redirect_to products_path, notice: 'Details updated correctly'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+
+    end
+
+
     def product_params
-        params.require(:product).permit(:title, :description, :price)
+        params.require(:product).permit(:title, :description, :price, :photo)
     end
 
 
